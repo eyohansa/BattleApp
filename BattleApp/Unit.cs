12aec7676904bd664afc7098da4a476f92a6df42
Eric Yohansa <eric.yohansa@outlook.com>
2019-09-05 05:18:10 +0700

    Support damage range.

--- BattleApp/Unit.cs
+++ BattleApp/Unit.cs
@@ -10,6 +10,15 @@ namespace BattleApp
     {
         public string Name { get; set; } = "Unknown";
         public int Health { get; set; }
-        public int Damage { get; set; }
+        public int MinDamage { get; set; } = 1;
+        public int MaxDamage { get; set; } = 1;
+
+        public void Attack(Unit target)
+        {
+            var random = new Random();
+            var damage = random.Next(MinDamage, MaxDamage);
+            target.Health -= damage;
+            Console.WriteLine($"{Name} dealth {damage} to {target.Name} (HP: {target.Health})");
+        }
     }
 }
