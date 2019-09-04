d705c83c04420f74ded76e2162cc92b07efc814b
Eric Yohansa <eric.yohansa@outlook.com>
2019-09-05 05:02:55 +0700

    Add combat log.

--- BattleApp/Game.cs
+++ BattleApp/Game.cs
@@ -21,8 +21,13 @@ namespace BattleApp
         public void DoRound()
         {
             round++;
+            Console.WriteLine($"Round {round} has started.");
+
             units[0].Health -= units[1].Damage;
             units[1].Health -= units[0].Damage;
+
+            Console.WriteLine($"{units[0].Name} dealt {units[0].Damage} to {units[1].Name} (HP: {units[1].Health})");
+            Console.WriteLine($"{units[1].Name} dealt {units[1].Damage} to {units[0].Name} (HP: {units[0].Health})");
         }
 
         public bool CheckForWin() => units[0].Health <= 0 || units[1].Health <= 0;
